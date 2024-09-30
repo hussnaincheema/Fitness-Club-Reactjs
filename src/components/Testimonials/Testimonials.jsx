948a61db8942f6c4d6208f70676ce430b5e4f30e
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2024-09-30 08:04:17 +0500

    Create Testimonials

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./Testimonials.css";
+import { testimonialsData } from "../../data/testimonialsData";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
+import { motion } from "framer-motion";
+
+const Testimonials = () => {
+  const transition = { type: "spring", duration: 3 };
+  const [selected, setSelected] = useState(0);
+  const tlength = testimonialsData.length;
+
+  return (
+    <div className="Testimonials" id="testimonials">
+      <div className="left_t">
+        <span>Testimonials</span>
+        <span className="stroke_text">What they</span>
+        <span>say about us</span>
+
+        <motion.span
+          key={selected}
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 100 }}
+          transition={transition}
+        >
+          {testimonialsData[selected].review}
+        </motion.span>
+        <span>
+          <span className="t_name">{testimonialsData[selected].name}</span> -{" "}
+          {testimonialsData[selected].status}
+        </span>
+      </div>
+
+      <div className="right_t">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ ...transition, duration: 2 }}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ ...transition, duration: 2 }}
+        ></motion.div>
+        <motion.img
+          key={selected}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={transition}
+          src={testimonialsData[selected].image}
+          alt="Testimonials Image"
+        />
+
+        <div className="arrows">
+          <img
+            onClick={() => {
+              selected === 0
+                ? setSelected(tlength - 1)
+                : setSelected((prev) => prev - 1);
+            }}
+            src={leftArrow}
+            alt="Left Arrow"
+          />
+          <img
+            onClick={() => {
+              selected === tlength - 1
+                ? setSelected(0)
+                : setSelected((prev) => prev + 1);
+            }}
+            src={rightArrow}
+            alt="Right Arrow"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
